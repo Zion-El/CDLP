4c4d6f900744a4ff975744cc2784829e51d707b6
Michael Gbadebo <mchladelola@gmail.com>
2023-05-11 12:26:26 +0100

    upworking customerUser

---
+++ autobiz/models.py
@@ -0,0 +1,237 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser, UserManager
+from django.core.validators import MinValueValidator
+from django.db import models, transaction
+
+
+# USER BANKS
+Bank = (
+    ("Access Bank", "Access Bank"),
+    ("Access(Diamond) Bank", "Access (Diamond) Bank"),
+    ("ECO Bank", "ECO Bank"),
+    ("First Bank of Nigeria", "First Bank of Nigeria"),
+    ("FCMBank", "FCMBank"),
+    ("FIdelity Bank", "FIdelity Bank"),
+    ("GTBank", "GTBank"),
+    ("Heritage Bank", "Heritage Bank"),
+    ("Kuda Bank", "Kuda Bank"),
+    ("Opay", "Opay"),
+    ("Palmpay", "Palmpay"),
+    ("Polarise Bank", "Polarise Bank"),
+    ("Stanbic IBTC", "Stanbic IBTC"),
+    ("Sterling Bank", "Sterling Bank"),
+    ("UBA", "UBA"),
+    ("Union Bank", "Union Bank"),
+    ("Unity Bank", "Unity Bank"),
+    ("Wema Bank", "Wema Bank"),
+    ("Zenith Bank", "Zenith Bank"),
+)
+
+# USER TYPE
+user_type = (
+    ("Smart Earner", "Smart Earner"),
+    ("Affilliate", "Affilliate"),
+    ("TopUser", "TopUser"),
+    ("API", "API"),
+)
+
+
+class CustomUserManager(UserManager):
+    def get_by_natural_key(self, username):
+        case_insensitive_username_field = "{}__iexact".format(self.model.USERNAME_FIELD)
+        return self.get(**{case_insensitive_username_field: username})
+
+# Create your models here.
+class CustomUser(AbstractUser):
+    objects = CustomUserManager()
+    email = models.EmailField()
+    FullName = models.CharField(max_length=200, null=True)
+    Address = models.CharField(max_length=500, null=True)
+    BankName = models.CharField(max_length=100, choices=Bank, blank=True)
+    AccountNumber = models.CharField(max_length=40, blank=True)
+    Phone = models.CharField(max_length=30, blank=True)
+    AccountName = models.CharField(max_length=200, blank=True)
+    Account_Balance = models.FloatField(default=0.00, null=True, validators=[MinValueValidator(0.0)],)
+    pin = models.CharField(null=True, blank=True, max_length=5)
+    referer_username = models.CharField(max_length=50, blank=True, null=True)
+    first_payment = models.BooleanField(default=False)
+    Referer_Bonus = models.FloatField(default=0.00, null=True, validators=[MinValueValidator(0.0)],)
+    user_type = models.CharField(max_length=30, choices=user_type, default="Smart Earner", null=True)
+    reservedaccountNumber = models.CharField(max_length=100, blank=True, null=True)
+    reservedbankName = models.CharField(max_length=100, blank=True, null=True)
+    reservedaccountReference = models.CharField(max_length=100, blank=True, null=True)
+    Bonus = models.FloatField(default=0.00, null=True, validators=[MinValueValidator(0.0)],)
+    verify = models.BooleanField(default=False)
+    email_verify = models.BooleanField(default=False)
+    DOB = models.DateField(null=True, blank=True,)
+    Gender = models.CharField(max_length=6, null=True,)
+    State_of_origin = models.CharField(max_length=100, null=True,)
+    Local_gov_of_origin = models.CharField(max_length=100, null=True,)
+    BVN = models.CharField(max_length=50, null=True,)
+    passport_photogragh = models.ImageField(upload_to="passport_images", null=True, help_text="Maximum of 50kb in size")
+    accounts = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.username
+
+    # def f_account(self):
+    #     try:
+    #         return json.loads(self.accounts)
+    #     except:
+    #         return {"accounts": []}
+
+    # def passport(self):
+    #     if self.passport_photogragh:
+    #         return "https://www.legitdata.com.ngmedia/%s" % (self.passport_photogragh)
+    #     else:
+    #         return "https://png.pngtree.com/png-vector/20190704/ourmid/pngtree-businessman-user-avatar-free-vector-png-image_1538405.jpg"
+
+
+
+    # def image_tag(self):
+    #     from django.utils.html import mark_safe
+
+    #     return mark_safe(
+    #         '<img src="https://www.legitdata.com.ngmedia/%s" width="150" height="150" />'
+    #         % (self.passport_photogragh)
+    #     )
+
+    # image_tag.short_description = "Image"
+
+    # def walletb(self):
+    #     return str(round(self.Account_Balance))
+
+    # def bonusb(self):
+    #     return str(round(self.Referer_Bonus))
+
+    # def ref_deposit(self, amount):
+    #     self.Referer_Bonus += amount
+    #     self.Referer_Bonus = round(self.Referer_Bonus, 2)
+    #     self.save()
+
+    # def ref_withdraw(self, amount):
+    #     if self.self.Referer_Bonus > amount or amount < 0:
+    #         return False
+    #     self.Referer_Bonus -= amount
+    #     self.Referer_Bonus = round(self.Referer_Bonus, 2)
+    #     self.save()
+
+    # @classmethod
+    # def withdraw(cls, id, amount):
+    #     with transaction.atomic():
+    #         account = cls.objects.select_for_update().get(id=id)
+    #         # print(account)
+    #         balance_before = account.Account_Balance
+    #         if account.Account_Balance < amount or amount < 0:
+    #             return False
+    #         account.Account_Balance -= amount
+    #         account.save()
+
+    #     try:
+    #         Transactions.objects.create(
+    #             user=CustomUser.objects.get(id=id),
+    #             transaction_type="DEBIT",
+    #             balance_before=balance_before,
+    #             balance_after=balance_before - amount,
+    #             amount=amount,
+    #         )
+
+    #     except:
+    #         pass
+
+    # @classmethod
+    # def deposit(cls, id, amount, transfer=False, medium="NONE"):
+    #     with transaction.atomic():
+    #         account = cls.objects.select_for_update().get(id=id)
+    #         # if transfer == False:
+    #         #     if account.referer_username:
+    #         #         if CustomUser.objects.filter(username__iexact=account.referer_username).exists():
+    #         #             if account.first_payment == False:
+    #         #                 referer = CustomUser.objects.get(
+    #         #                     username__iexact=account.referer_username)
+    #         #                 if amount >= 5000:
+    #         #                     referer.ref_deposit(100)
+    #         #                     account.first_payment = True
+    #         #                 else:
+    #         #                     referer.ref_deposit((0.05*amount))
+    #         #                     account.first_payment = True
+
+    #         balance_before = account.Account_Balance
+    #         if medium != "NONE":
+    #             if ChargeUser.objects.filter(username=account.username).exists():
+    #                 pending_charge = ChargeUser.objects.filter(
+    #                     username=account.username
+    #                 ).last()
+    #                 if pending_charge.pending_amount > 0:
+    #                     if amount > pending_charge.pending_amount:
+    #                         amt = amount - pending_charge.pending_amount
+    #                         balance_before = account.Account_Balance
+    #                         pending_charge.balance_before = balance_before
+    #                         account.Account_Balance += amt
+    #                         account.save()
+
+    #                         try:
+    #                             WalletSummary.objects.create(
+    #                                 user=account,
+    #                                 product=f"N{pending_charge.pending_amount} pending wallet charge paid",
+    #                                 amount=amount,
+    #                                 previous_balance=balance_before,
+    #                                 after_balance=balance_before + amount,
+    #                             )
+    #                         except:
+    #                             pass
+    #                         pending_charge.comment = f"N{pending_charge.pending_amount} pending wallet charge paid"
+    #                         pending_charge.balance_after = amt
+    #                         pending_charge.pending_amount = 0
+    #                         pending_charge.save()
+
+    #                     else:
+    #                         balance_before = account.Account_Balance
+    #                         pending_charge.balance_before = balance_before
+    #                         amt = pending_charge.pending_amount - amount
+    #                         pending_charge.comment = f"N{amount} paid from  pending wallet charge, pending amount remain {amount} "
+    #                         pending_charge.pending_amount = amt
+    #                         pending_charge.balance_after = 0
+    #                         pending_charge.save()
+
+    #                 else:
+    #                     account.Account_Balance += amount
+    #                     account.save()
+    #             else:
+    #                 account.Account_Balance += amount
+    #                 account.save()
+
+    #         try:
+    #             Transactions.objects.create(
+    #                 user=CustomUser.objects.get(id=id),
+    #                 transaction_type="CREDIT",
+    #                 balance_before=balance_before,
+    #                 balance_after=balance_before + amount,
+    #                 amount=amount,
+    #             )
+
+    #         except:
+    #             pass
+
+    #         try:
+    #             WalletFunding.objects.create(
+    #                 user=CustomUser.objects.get(id=id),
+    #                 medium=medium,
+    #                 previous_balance=balance_before,
+    #                 after_balance=balance_before + amount,
+    #                 amount=amount,
+    #             )
+
+    #         except:
+    #             pass
+
+    #     return account
+
+    # def ref_withdraw(self, amount):
+    #     if self.Referer_Bonus > 0.0:
+    #         self.Referer_Bonus -= amount
+    #         self.Referer_Bonus = round(self.Referer_Bonus, 2)
+    #         self.save()
+
+    class Meta:
+        verbose_name_plural = "USERS MANAGEMENT"
---
+++ autobiz/views.py
@@ -0,0 +1,177 @@
+from django.shortcuts import render
+from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib.sites.shortcuts import get_current_site
+from django.core.mail import EmailMessage
+from .models import CustomUser
+from django.views import generic
+from django.forms.utils import ErrorList
+from django import forms
+from django.shortcuts import get_object_or_404, redirect, render
+from django.template.loader import get_template
+from django.urls import reverse_lazy
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
+
+# Create your views here.
+
+
+
+
+# HANDLING SIGNUP
+class SignUp(SuccessMessageMixin, generic.CreateView):
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy("login")
+    template_name = "signup.html"
+    # success_messages = 'Please confirm your email address to complete the registration,activation link has been sent to your email, also check your email spam folder'
+    success_messages = "You have successfully Registered, Kindly login to continue"
+
+    def abc(self):
+        ref = ""
+        if "referal" in self.request.session:
+            ref = self.request.session["referal"]
+
+        return ref
+
+    def get_context_data(self, **kwargs):
+        context = super(SignUp, self).get_context_data(**kwargs)
+        context["referal_user"] = self.abc()
+
+        return context
+
+    def form_valid(self, form):
+        object = form.save(commit=False)
+        username = object.username
+        email = object.email
+        # object.email_verify = False
+        # object.is_active = False
+        user = object
+
+        # to check if the username is available or used
+        if CustomUser.objects.filter(username__iexact=object.username).exists():
+            form._errors[forms.forms.NON_FIELD_ERRORS] = ErrorList(
+                ["This username has been taken"]
+            )
+            return self.form_invalid(form)
+
+        # to check if email is available or used
+        elif CustomUser.objects.filter(email__iexact=object.email).exists():
+            form._errors[forms.forms.NON_FIELD_ERRORS] = ErrorList(
+                ["This email has been taken"]
+            )
+            return self.form_invalid(form)
+        elif CustomUser.objects.filter(Phone__iexact=object.Phone).exists():
+            form._errors[forms.forms.NON_FIELD_ERRORS] = ErrorList(
+                ["This Phone has been taken"]
+            )
+            return self.form_invalid(form)
+
+        elif not object.email.endswith(("@gmail.com", "@yahoo.com")):
+            form._errors[forms.forms.NON_FIELD_ERRORS] = ErrorList(
+                ["We accept only valid gmail or yahoo mail account"]
+            )
+            return self.form_invalid(form)
+
+        elif object.referer_username:
+            if CustomUser.objects.filter(
+                username__iexact=object.referer_username
+            ).exists():
+                referal_user = CustomUser.objects.get(
+                    username__iexact=object.referer_username
+                )
+
+            else:
+                object.referer_username = None
+
+        form.save()
+
+        # try:
+        #     current_site = get_current_site(self.request)
+        #     mail_subject = "Activate your legitdata account."
+        #     message = {
+        #         "user": user,
+        #         "domain": current_site.domain,
+        #         "uid": urlsafe_base64_encode(force_bytes(user.pk)),
+        #         "token": account_activation_token.make_token(user),
+        #     }
+        #     message = get_template("acc_active_email.html").render(message)
+        #     to_email = email
+        #     email = EmailMessage(mail_subject, message, to=[to_email])
+        #     email.content_subtype = "html"
+        #     email.send()
+
+        # except:
+        #     pass
+        # try:
+        #     Referal_list.objects.create(user=referal_user, username=username)
+        # except:
+        #     pass
+        # try:
+        #     # messages.success( self.request, 'Please confirm your email address to complete the registration,activation link has been sent to your email,, also check your email spam folder')
+
+        #     sendmail(
+        #         "Welcome to legitdata.com.ng",
+        #         "Welcome to alegitdata.com.ngWe offer instant recharge of Airtime, Databundle, CableTV (DStv, GOtv & Startimes), Electricity Bill Payment and Airtime to Cash.",
+        #         email,
+        #         username,
+        #     )
+
+        # except:
+        #     pass
+        # try:
+
+        #     def create_id():
+        #         random.randint(1, 10)
+        #         num_2 = random.randint(1, 10)
+        #         num_3 = random.randint(1, 10)
+        #         return str(num_2) + str(num_3) + str(uuid.uuid4())[:4]
+
+        #     body = {
+        #         "accountReference": create_id(),
+        #         "accountName": username,
+        #         "currencyCode": "NGN",
+        #         "contractCode": f"{get_config().monnify_contract_code}",
+        #         "customerEmail": email,
+        #         "incomeSplitConfig": [],
+        #         "restrictPaymentSource": False,
+        #         "allowedPaymentSources": {},
+        #     }
+
+        #     if not email:
+        #         data = json.dumps(body)
+        #         ad = requests.post(
+        #             "https://api.monnify.com/api/v1/auth/login",
+        #             auth=HTTPBasicAuth(
+        #                 f"{get_config().monnify_API_KEY}",
+        #                 f"{get_config().monnify_SECRET_KEY}",
+        #             ),
+        #         )
+        #         mydata = json.loads(ad.text)
+
+        #         headers = {
+        #             "Content-Type": "application/json",
+        #             "Authorization": "Bearer {}".format(
+        #                 mydata["responseBody"]["accessToken"]
+        #             ),
+        #         }
+        #         ab = requests.post(
+        #             "https://api.monnify.com/api/v1/bank-transfer/reserved-accounts",
+        #             headers=headers,
+        #             data=data,
+        #         )
+
+        #         mydata = json.loads(ab.text)
+
+        #         user = CustomUser.objects.get(email__iexact=email)
+
+        #         user.reservedaccountNumber = mydata["responseBody"]["accountNumber"]
+        #         user.reservedbankName = mydata["responseBody"]["bankName"]
+        #         user.reservedaccountReference = mydata["responseBody"][
+        #             "accountReference"
+        #         ]
+        #         user.save()
+
+        #     else:
+        #         pass
+
+        # except:
+        #     pass
+        return super(SignUp, self).form_valid(form)
