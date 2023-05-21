39f25081f8dbeede10b7a8dd61d349dc5dd72bea
Huzzy619 <blazingkrane@gmail.com>
2023-05-21 20:46:57 +0100

    ....

--- core/urls.py
+++ core/urls.py
@@ -5,9 +5,10 @@ from . import views
 urlpatterns = [
     path('login/', views.LoginView.as_view()), 
     path('register/', views.RegisterView.as_view()),
-    path('refresh/token', views.RefreshView.as_view()), 
+    path('refresh/token/', views.RefreshView.as_view()), 
     path('update/', views.DetailUpdateView.as_view(), name='update'),
     path('confirm/<int:user_id>/', views.ConfirmationView.as_view(), name='confirm_registration'),
-    path('login/google', views.GoogleSocialAuthView.as_view()),
+    path('login/google/', views.GoogleSocialAuthView.as_view()),
     
 ]
+ 
